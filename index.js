1cdcc266d4f0cf709ffadfbfdf1985979ee3a55a
Bernard Sidney Robinson <sidneyjr.robinson@gmail.com>
2021-07-28 17:50:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// onclick functionality below
+function xbox() {
+  document.getElementById("xbox").onclick = alert("You are elite!");
+}
+function playstation(){
+  document.getElementById("playstation").onclick = alert("Not a real gamer!")
+}
+function nintendo(){
+  document.getElementById("nintendo").onclick = alert("Do you even game, bro?")
+}
+
