ccdf948c28982bad903243537bc043c9e59516eb
Bernard Sidney Robinson <sidneyjr.robinson@gmail.com>
2021-08-02 14:15:13 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // onclick functionality below
 function xbox() {
   document.getElementById("xbox").onclick = alert("You are elite!");
+  window.location="https://www.xbox.com/en-US";
 }
 function playstation(){
-  document.getElementById("playstation").onclick = alert("Not a real gamer!")
+  document.getElementById("playstation").onclick = alert("Not a real gamer!");
+  window.location="https://www.playstation.com/en-us/";
 }
 function nintendo(){
-  document.getElementById("nintendo").onclick = alert("Do you even game, bro?")
+  document.getElementById("nintendo").onclick = alert("Do you even game, bro?");
+  window.location="https://www.nintendo.com/";
 }
-
+// end of function
